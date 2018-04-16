4874e57f4b521c3b2fe0f8053aa16bf5494799ff
TUPOBO <1028461931@qq.com>
2018-04-16 16:10:21 +0800

    add onPullDownReFresh

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,51 +1,99 @@
-const newsTypes = [
+let newsTypes = [
   {
+    id: 0,
     type: 'gn',
     name: '国内',
-    active: true
+    active: false
   },
   {
+    id: 1,
     type: 'gj',
     name: '国际',
     active: false
   },
   {
+    id: 2,
     type: 'cj',
     name: '财经',
     active: false
   },
   {
+    id: 3,
     type: 'yl',
     name: '娱乐',
     active: false
   },
   {
+    id: 4,
     type: 'js',
     name: '军事',
     active: false
   },
   {
+    id: 5,
     type: 'ty',
     name: '体育',
     active: false
   },
   {
+    id: 6,
     type: 'other',
     name: '其他',
     active: false
   }
 ]
 
+const newsListIndex = ['gn', 'gj', 'cj', 'yl', 'js', 'ty', 'other']
+
 Page({
   data: {
-    newsTypes: newsTypes,
+    newsType: newsTypes,
+    activeNewsType: [],
     newsInfo: []
   },
 
-  onLoad: function (options) {
-    this.getNewsInfo(newsTypes[0].type)
+  /**
+   *
+   *
+   * @param {any} options
+   */
+  onLoad: function () {
+    let type = newsTypes[0].type
+    this.setNewsActiveType(type)
+    this.getNewsInfo(type)
   },
 
+  onPullDownRefresh: function () {
+    for (let type of newsTypes) {
+      if (type.active) {
+        console.log(type.type)
+        this.setNewsActiveType(type.type)
+        this.getNewsInfo(type.type)
+      }
+    }
+    console.log('dfdas')
+  },
+
+  setNewsActiveType: function (type) {
+    let index = newsListIndex.indexOf(type)
+    newsTypes[index].active = true
+    for (let type of newsTypes) {
+      if (type.id === index) {
+        type.active = true
+      } else {
+        type.active = false
+      }
+    }
+    this.setData({
+      newsType: newsTypes
+    })
+  },
+
+  /**
+   *
+   *
+   * @param {any} type
+   */
   getNewsInfo: function (type) {
     wx.request({
       url: 'https://test-miniprogram.com/api/news/list',
@@ -76,7 +124,8 @@ Page({
   },
 
   changeNewsType: function (event) {
-    console.log(event)
-    this.getNewsInfo(event.currentTarget.dataset.type)
+    let type = event.currentTarget.dataset.type
+    this.setNewsActiveType(type)
+    this.getNewsInfo(type)
   }
 })
